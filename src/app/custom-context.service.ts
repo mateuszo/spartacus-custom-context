81d35beeb8eeba70d98c04745dd9e8e9e6c51a51
Mateusz Ostafil <mostafil@divante.com>
2020-11-30 12:07:53 +0100

    Re-use built in context selector

--- src/app/custom-context.service.ts
+++ src/app/custom-context.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { CurrencyService, LanguageService, SiteContext } from '@spartacus/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { SITE_CONTEXTS } from './available-contexts';
 
+
+const CONTEXTS = SITE_CONTEXTS.map(name => ({ isocode: name }));
 @Injectable({
   providedIn: 'root'
 })
@@ -10,14 +13,14 @@ export class CustomContextService implements SiteContext<string> {
 
   constructor(private languageService: LanguageService, private currencyService: CurrencyService) { }
 
-  private currentContext = new BehaviorSubject<string>(SITE_CONTEXTS[0]);
+  private currentContext$ = new BehaviorSubject<any>(CONTEXTS[0]);
 
-  getAll(): Observable<string[]> {
-    return of(SITE_CONTEXTS);
+  getAll(): Observable<any[]> {
+    return of(CONTEXTS);
   }
 
-  getActive(): Observable<string> {
-    return this.currentContext;
+  getActive(): Observable<any> {
+    return this.currentContext$.pipe(map(ctx => ctx.isocode));
   }
 
   setActive(isocode: string): void {
@@ -27,7 +30,7 @@ export class CustomContextService implements SiteContext<string> {
     this.languageService.setActive(params[0]);
     this.currencyService.setActive(params[1]);
 
-    this.currentContext.next(isocode);
+    this.currentContext$.next({ isocode });
   }
 
 
